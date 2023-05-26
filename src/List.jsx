950f5093dfb8913153dec47942aaf9a99d953349
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-05-26 21:12:53 +0300

    img,name,age rendered

--- src/List.jsx
+++ src/List.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 // accept props here
-const List = (props) => {
-	const { people } = props
-	// Map over prop "people" and code the right structure
+const List = ({ people }) => {
+  // Map over prop "people" and code the right structure
 
-	return people.map((person) => {
-		const { id, name, age, image } = person
-		return <article />
-	})
-}
+  return people.map((person) => {
+  return  <article className="person" key={person.id}>
+      <img src={person.image} alt={person.name} />
+      <div>
+        <h4>{person.name}</h4>
+        <p>{person.age} Years</p>
+      </div>
+    </article>;
+  });
+};
 
-export default List
+export default List;
