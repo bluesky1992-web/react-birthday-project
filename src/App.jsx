165621a36dcb66c1fa4c105425576873205a7982
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-05-29 17:42:11 +0300

    tutorial done the clear button clears the people

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import React, { useState } from 'react'
-import data from './data'
-import List from './List'
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+// import { useState } from 'react';
 function App() {
-	// create a state variable here
-
-	// this should clear all records
-	function clearAllRecords() {}
-	return (
-		<main>
-			<section className="container">
-				{/* change this to state variable data */}
-				<h3>{[].length} birthdays today</h3>
-				<List people={[]} />
-				<button onClick={clearAllRecords}>Clear All</button>
-			</section>
-		</main>
-	)
+  // create a state variable here
+  const [people, setPeople] = useState(data);
+  // this should clear all records
+  function clearAllRecords() {
+	setPeople([])
+  }
+  return (
+    <main>
+      <section className="container">
+        {/* change this to state variable data */}
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={clearAllRecords}>Clear All</button>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
